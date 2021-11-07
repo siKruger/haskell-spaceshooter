21ba12d8c0038172b97b37f22c62cc34382d8bf5
siKruger <eddiwood2@gmail.com>
2021-11-07 22:10:13 +0100

    asteroids

--- app/Main.hs
+++ app/Main.hs
@@ -5,33 +5,30 @@ module Main where
 import Graphics.Gloss
 
 import Graphics.Gloss.Interface.Pure.Game
+import Data.Fixed (Pico)
 
-data MoveDirection
-  = East
-  | West
-  | None
+data MoveDirection = East | West | North | South | None
   deriving (Eq)
-
-data Heading
-  = FacingWest
-  | FacingEast
-  deriving (Eq)
-
-data GameState =
-  GameState
-    { position :: Point
-    , direction :: MoveDirection
-    , heading :: Heading
-    , currentLevel :: Level
-    , speedX :: Float
-    , speedY :: Float
-    }
 
 type CellType = Char
 
 type Cell = (Point, CellType)
 
 type Level = [Cell]
+
+type Asteroid = Point
+
+data GameState =
+  GameState
+    { position :: Point
+    , direction :: MoveDirection
+    , currentLevel :: Level
+    , livesLeft :: Int
+    , speedX :: Float
+    , speedY :: Float
+    , asteroids :: [Asteroid]
+    }
+
 
 tileSize :: Float
 tileSize = 32.0
@@ -40,16 +37,10 @@ window :: Display
 window = InWindow "Space Shooter" (500, 700) (50, 50)
 
 background :: Color
-background = makeColor 0.2 0.1 0.1 1
+background = makeColor 0.033 0.04 0.26 1
 
 fps :: Int
 fps = 60
-
---Haben wir eine Karotte?
-isHit :: Point -> Point -> Bool
-isHit (b1x, b1y) (b2x, b2y) =
-  (b1x - 10) < b2x + tileSize &&
-  b1x + 50 - 10 > b2x && b1y < b2y + tileSize && b1y + 54 > b2y
 
 -- Eine Zeile bauen
 makeRow :: String -> Int -> Level
@@ -78,103 +69,83 @@ drawTile :: Cell -> Picture -> Picture -> Picture
 drawTile cell tileImg foodImg =
   uncurry translate (fst cell) (whatImg cell tileImg foodImg)
 
-isRight :: Heading -> Int
-isRight FacingEast = 6
-isRight _ = 0
 
-incSprite :: GameState -> Int
-incSprite gs = 1
 
+
+-- Key Input abarbeiten und handeln
 handleKeys :: Event -> GameState -> GameState
-handleKeys (EventKey (SpecialKey KeyLeft) Down _ _) gs =
-  gs {direction = West, heading = FacingWest}
-handleKeys (EventKey (SpecialKey KeyRight) Down _ _) gs =
-  gs {direction = East, heading = FacingEast}
-handleKeys (EventKey (SpecialKey KeySpace) Down _ _) gs =
-  gs
-    { speedY =
-        if isCollision gs (fst (position gs), snd (position gs) + speedY gs) '*'
-          then 6
-          else (-6)
-    }
+handleKeys (EventKey (SpecialKey KeyLeft) Down _ _) gs = gs {direction = West}
+handleKeys (EventKey (SpecialKey KeyRight) Down _ _) gs = gs {direction = East}
+handleKeys (EventKey (SpecialKey KeyUp ) Down _ _) gs = gs {direction = North}
+handleKeys (EventKey (SpecialKey KeyDown  ) Down _ _) gs = gs {direction = South}
 handleKeys _ gs = gs {direction = None}
 
-checkFood :: GameState -> Level
-checkFood gs =
-  filter
-    (\cell -> not (isHit (fst cell) (position gs) && snd cell == '%'))
-    (currentLevel gs)
 
-checkSpeedY :: GameState -> Float
-checkSpeedY gs
-  | isCollision gs (fst (position gs), snd (position gs) + speedY gs) '*' = -3
-  | speedY gs >= -6 = speedY gs - 0.1
-  | otherwise = -6
 
-checkSpeedX :: GameState -> Float
-checkSpeedX gs
-  | direction gs == West || direction gs == East =
-    if speedX gs > 5.0
-      then 5.0
-      else speedX gs + 0.5
-  | otherwise =
-    if speedX gs <= 0
-      then 0
-      else speedX gs - 0.5
+{-
+Kollision mit der Wand unserer Spielwelt
 
-isCollision :: GameState -> Point -> CellType -> Bool
-isCollision gs pnt checkType =
-  any
-    (\((x, y), tileType) -> tileType == checkType && isHit pnt (x, y))
-    (currentLevel gs)
+todo: refactor in eine methode mit pattern matching
+-}
+isCollisionWithGameWallRight :: Point -> Bool
+isCollisionWithGameWallRight pnt = if(fst (pnt) < 200) then True else False
 
-moveX :: MoveDirection -> GameState -> Point
-moveX East gs =
-  if not (isCollision gs (fst (position gs) + speedX gs, snd (position gs)) '*')
-    then (fst (position gs) + speedX gs, snd (position gs))
-    else position gs
-moveX West gs =
-  if not
-       (isCollision
-          gs
-          (fst (position gs) + speedX gs * (-1), snd (position gs))
-          '*')
-    then (fst (position gs) + speedX gs * (-1), snd (position gs))
-    else position gs
-moveX _ gs =
-  if speedX gs > 0 &&
-     not
-       (isCollision
-          gs
-          ( fst (position gs) +
-            speedX gs *
-            (if heading gs == FacingWest
-               then (-1)
-               else 1)
-          , snd (position gs))
-          '*')
-    then ( fst (position gs) +
-           speedX gs *
-           (if heading gs == FacingWest
-              then (-1)
-              else 1)
-         , snd (position gs))
-    else position gs
+isCollisionWithGameWallLeft :: Point -> Bool
+isCollisionWithGameWallLeft pnt = if(fst (pnt) > -200) then True else False
 
-moveY :: GameState -> Point -> Point
-moveY gs pnt =
-  if not (isCollision gs (fst pnt, snd pnt + speedY gs) '*')
-    then (fst pnt, snd pnt + speedY gs)
-    else pnt
+isCollisionWithGameWallUp :: Point -> Bool
+isCollisionWithGameWallUp pnt = if(snd (pnt) < 300) then True else False 
+
+isCollisionWithGameWallDown :: Point -> Bool
+isCollisionWithGameWallDown pnt = if(snd (pnt) > -300) then True else False 
+
+
+
+{- 
+fst, snd geben das erste/letzte Element einer Liste zurück
+-}
+movePlayer :: MoveDirection -> GameState -> Point
+movePlayer East gs = 
+  if(isCollisionWithGameWallRight (fst (position gs), snd (position gs))) 
+    then  
+    (fst (position gs) + speedX gs, snd (position gs))
+    else
+      (fst (position gs), snd (position gs))
+  
+movePlayer West gs =
+  if(isCollisionWithGameWallLeft (fst (position gs), snd (position gs))) 
+    then
+      (fst (position gs) + speedX gs * (-1), snd (position gs))
+    else
+      (fst (position gs), snd (position gs))
+
+movePlayer North gs =
+  if(isCollisionWithGameWallUp (fst (position gs), snd (position gs)))
+    then
+      (fst (position gs), snd (position gs) + speedY gs * (-1))
+    else 
+      (fst (position gs), snd (position gs))
+
+
+movePlayer South gs =
+  if(isCollisionWithGameWallDown (fst (position gs), snd (position gs)))
+    then
+      (fst (position gs), snd (position gs) + speedY gs * 1)
+    else 
+      (fst (position gs), snd (position gs))
+
+
+movePlayer _ gs = (fst (position gs), snd (position gs))
+
 
 -- Update Funktion
 update :: Float -> GameState -> GameState
 update _ gs =
   gs
-    { speedY = checkSpeedY gs
-    , speedX = checkSpeedX gs
-    , position = moveY gs $ moveX (direction gs) gs
-    , currentLevel = checkFood gs
+    { speedY = -5
+    , speedX = 5
+    , position = movePlayer (direction gs) gs
+    , asteroids = [(fst aste, snd aste - 1) | aste <- asteroids gs]
     }
 
 main :: IO ()
@@ -189,9 +160,10 @@ main = do
           { position = (0.0, 0.0)
           , direction = None
           , currentLevel = level
-          , heading = FacingWest
+          , livesLeft = 5
           , speedX = 0
-          , speedY = (-6)
+          , speedY = 0
+          , asteroids = [(50, 50)]
           }
   play
     window
@@ -208,12 +180,31 @@ main = do
 
 -- render Methode von Gloss
 render :: GameState -> [Picture] -> Picture
-render gs imgs =
-  pictures
-    ([drawTile cell (head imgs) (imgs !! 1) | cell <- currentLevel gs] 
-    ++
-     [ translate
+render gs imgs = pictures
+    ([drawTile cell (head imgs) (imgs !! 1) | cell <- currentLevel gs
+    ] ++ [ translate
          (fst (position gs))
-         (snd (position gs) + 10)
+         (snd (position gs))
          (imgs !! 2) -- Position 2 des render arrays stellt den Spieler da
-     ])
+     ] ++ [
+       drawLivesLeft gs
+       ] ++ [
+         drawAsteroids gs imgs
+       ])
+
+{-
+  Liefert alle Asteroiden als Picture
+-}
+drawAsteroids :: GameState -> [Picture] -> Picture
+drawAsteroids gs imgs = pictures [(translate (fst aste) (snd aste) (imgs !! 1)) | aste <- asteroids gs]
+
+
+{-
+  Zeigt die Verbleibenden Leben an. 
+-}
+drawLivesLeft :: GameState -> Picture 
+drawLivesLeft gs = Color (makeColor 1 1 1 1) (pictures([
+    translate (-150) (300) (Scale (0.3) (0.3) (Text "Leben"))
+  ] ++ [
+    translate (-200) (300) (Scale (0.3) (0.3) (Text (show (livesLeft gs))))
+  ]))
